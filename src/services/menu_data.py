83cf2fa6d5a49934384d704936f5029d2e36f7db
Kauan <kauan.s.leite@hotmail.com>
2023-04-13 18:17:02 -0300

    3° Requisito

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
+import pandas as pd
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        data = pd.read_csv(source_path)
+        self.dishes = set()
+
+        for dish, price in data[["dish", "price"]].itertuples(index=False):
+            self.dishes.add(Dish(dish, price))
+
+        for dish, ingredient, recipe_amount in data[
+            ["dish", "ingredient", "recipe_amount"]
+                ].itertuples(index=False):
+            for item in self.dishes:
+                if item.name == dish:
+                    item.add_ingredient_dependency(
+                        Ingredient(ingredient), recipe_amount
+                        )
