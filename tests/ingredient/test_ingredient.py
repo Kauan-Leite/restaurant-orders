d30800ba1462842cbce8f4209f50612b9c32a1e8
Kauan <kauan.s.leite@hotmail.com>
2023-04-13 15:21:34 -0300

    1° Requisito

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
+# import pytest
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    frango = Ingredient('frango')
+    another_frango = Ingredient('frango')
+    ovo = Ingredient('ovo')
+
+    assert frango.__eq__(another_frango)
+    assert frango.__hash__() != ovo.__hash__()
+    assert frango.__hash__() == another_frango.__hash__()
+    assert frango.__repr__() == "Ingredient('frango')"
+    assert frango.name == 'frango'
+    assert len(frango.restrictions) == 2
